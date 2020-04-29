fa7b77bac9c4824d136dd23cfd2df1e7d0930f21
Samuel Gaborieau <sam.gabor@hotmail.com>
2020-04-29 02:01:52 +0200

    initial commit

---
+++ src/Ajax/Response.php
@@ -0,0 +1,197 @@
+<?php
+
+use Ajax\Util\StatusText;
+
+namespace Ajax;
+
+class Response {
+
+    private static $isAutoStatusCode = true;
+    private static $isAutoStatusText = true;
+
+    private static $nameAutoStatusCode = "statusCode";
+    private static $nameAutoStatusText = "statusText";
+
+    private static $details = [];
+
+    private $response = [];
+    private $code = 200;
+
+    private $isReady = false;
+
+    public static function getDetails(): array {
+
+        return self::$details;
+    }
+
+    public static function addDetails( string $key, $val ): int {
+
+        self::$details[ $key ] = $val;
+
+        return \count( self::$details );
+    }
+
+    public static function deleteDetails( $key = NULL ) {
+
+        if( \is_null( $key )  ) {
+
+            self::$details = [] ;
+
+            return 0;
+
+        } else if(
+            \is_string( $key ) &&
+            isset( self::$details[ $key ] )
+        ) {
+
+            unset( self::$details[ $key ] ) ;
+
+            self::$details = \array_values( self::$details ) ;
+
+            return \count( self::$details ) ;
+        }
+
+        return false;
+    }
+
+    public static function getIsAutotStatusCode(): bool {
+
+        return self::$isAutoStatusCode;
+    }
+
+    public static function setIsAutotStatusCode( bool $isAutoStatusCode ): bool {
+
+        self::$isAutoStatusCode = $isAutoStatusCode;
+
+        return self::$isAutoStatusCode;
+    }
+
+    public static function getIsAutoStatusText(): bool {
+
+        return self::$isAutoStatusText;
+    }
+
+    public static function setIsAutoStatusText( bool $isAutoStatusText ): bool {
+
+        self::$isAutoStatusText = $isAutoStatusText;
+
+        return self::$isAutoStatusText;
+    }
+
+    public static function getNameAutoStatusCode(): string {
+
+        return self::$nameAutoStatusCode;
+    }
+
+    public static function setNameAutoStatusCode( string $nameAutoStatusCode ): string {
+
+        self::$nameAutoStatusCode = $nameAutoStatusCode;
+
+        return self::$nameAutoStatusCode;
+    }
+
+    public static function getNameAutoStatusText(): string {
+
+        return self::$nameAutoStatusText;
+    }
+
+    public static function setNameAutoStatusText( string $nameAutoStatusText ): string {
+
+        self::$nameAutoStatusText = $nameAutoStatusCode;
+    }
+
+    public function getResponse(): array {
+
+        return $this->response;
+    }
+
+    public function getCode(): int {
+
+        return $this->code;
+    }
+
+    public function addAutoValue() {
+
+        if( self::$isAutoStatusCode ) {
+
+            $this->response[ self::$nameAutoStatusCode ] = $this->code;
+        }
+
+        if( self::$isAutoStatusText ) {
+
+            $statusText = ( new StatusText( $this->code ) )->getStatusText();
+
+            if( !\is_null( $statusText ) ) {
+
+                $this->response[ self::$nameAutoStatusText ] = $statusText;
+            }
+        }
+
+    }
+
+    public function send( string $charset = "utf-8" ): void {
+
+        \header("Content-Type: application/json; charset={$charset}");
+
+        if( $this->isReady ) {
+
+            \http_response_code( (int) $this->code ) ;
+
+            echo \json_encode( $this->response ) ;
+
+        } else {
+
+            self::setDefault() ;
+
+            $this->prepare( [
+                "_response-auto-generate" => "you have attempt send response with AjaxResponse as bad response",
+                "_prepare" => "you should call `bool prepare( ?array response, ?int code )` method before call `void send( ?string charset )` method"
+            ], 500 ) ;
+
+            $this->send() ;
+        }
+    }
+
+    public function prepare( $response, int $code = 200 ): bool {
+
+        if( \is_string( $response ) ) {
+
+            $details = $response;
+            $response = [];
+            $response['details'] = $details;
+            unset( $details );
+
+        } else if( \is_null( $response ) ) {
+
+            $response = [];
+        }
+
+        if( \is_array( $response ) ) {
+
+            $this->response = $response;
+            $this->code = $code;
+
+            $this->addAutoValue();
+
+            $this->isReady = true;
+
+            return true;
+
+        } else {
+
+            $this->isReady = false;
+
+            return false;
+        }
+
+    }
+
+    private static function setDefault(): void {
+
+        self::$isAutoStatusCode = true;
+        self::$isAutoStatusText = true;
+        self::$nameAutoStatusCode = "statusCode";
+        self::$nameAutoStatusText = "statusText";
+    }
+
+} ;
---
+++ src/Ajax/Util/StatusText.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Ajax\Util;
+
+class StatusText {
+
+    const ASSOCIATE_STATUS_TEXT = [
+
+        "success" => [
+            "200" => "success",
+            "201" => "Created",
+            "202" => "Accepted",
+            "203" => "Non-Authoritative Information",
+            "204" => "No Content",
+            "205" => "Reset Content",
+            "206" => "Partial Content",
+            "207" => "Multi-Status",
+            "208" => "Already Reported",
+            "210" => "Content Different",
+            "216" => "IM Used"
+        ] ,
+        "redirect" => [
+            "300" => "Multiple Choices",
+            "301" => "Moved Permanently",
+            "302" => "Moved ( Found )",
+            "303" => "See Other",
+            "304" => "Not Modified",
+            "305" => "Use Proxy",
+            "306" => "Switch Proxy",
+            "307" => "Temporary Redirect",
+            "308" => "Permanent Redirect",
+            "310" => "Too Many Redirects"
+        ],
+        "error-browser" => [
+            "400" => "Bad Request",
+            "401" => "Unauthorized",
+            "402" => "Payment Required",
+            "403" => "Forbidden",
+            "404" => "Not Found",
+            "405" => "Method Not Allowed",
+            "406" => "Not Acceptable",
+            "407" => "Proxy Authentication Required",
+            "408" => "Request Time-out",
+            "409" => "Conflict",
+            "410" => "Gone",
+            "411" => "Length Required",
+            "412" => "Precondition Failed",
+            "413" => "Request Entity Too Large",
+            "414" => "Request-URI Too Long",
+            "415" => "Unsupported Media Type",
+            "416" => "Requested range unsatisfiable",
+            "417" => "Expectation failed",
+            "418" => "I’m a teapot", // RFC 2324 - 1er avril 1998, Hyper Text Coffee Pot Control Protocol.
+            "421" => "Bad mapping / Misdirected Request",
+            "422" => "Unprocessable entity",
+            "423" => "Locked",
+            "424" => "Method failure",
+            "425" => "Unordered Collection",
+            "426" => "Upgrade Required",
+            "428" => "Precondition Required",
+            "429" => "Too Many Requests",
+            "431" => "Request Header Fields Too Large",
+            "449" => "Retry With",
+            "450" => "Blocked by Windows Parental Controls",
+            "451" => "Unavailable For Legal Reasons",
+            "456" => "Unrecoverable Error"
+        ],
+        "error-server" => [
+            "500" => "Internal Server Error",
+            "501" => "Not Implemented",
+            "502" => "Bad Gateway ou Proxy Error",
+            "503" => "Service Unavailable",
+            "504" => "Gateway Time-out",
+            "505" => "HTTP Version not supported",
+            "506" => "Variant Also Negotiates",
+            "507" => "Insufficient storage",
+            "508" => "Loop detected",
+            "509" => "Bandwidth Limit Exceeded",
+            "510" => "Not extended",
+            "511" => "Network authentication required"
+        ]
+    ] ;
+
+    const ASSOCIATE_STATUS_TYPE = [
+        "2" => "success",
+        "3" => "redirect",
+        "4" => "error-browser",
+        "5" => "error-server"
+    ] ;
+
+    private $status = "200";
+
+    private $statusText = NULL;
+
+    private $statusType = NULL;
+
+    public function __construct( int $statusCode = 200 ) {
+
+        $this->status = (string) $statusCode;
+    }
+
+    public function getStatusText(): ?string {
+
+        $statusType = $this->getStatusType();
+
+        if( \is_null( $statusType ) ) {
+
+            return NULL;
+        }
+
+        $codesText = self::ASSOCIATE_STATUS_TEXT[ $statusType ] ;
+
+        return isset( $codesText[ $this->status ] ) ? $codesText[ $this->status ]: NULL;
+    }
+
+    public function getStatusType(): ?string {
+
+        if( !\is_null( $this->statusType ) ) {
+
+            return $this->statusType;
+        }
+
+        $statusType = NULL;
+
+        foreach( self::ASSOCIATE_STATUS_TYPE as $key => $val ) {
+
+            if( $this->status[0] === $key ) {
+
+                $statusType = $val;
+            }
+        }
+
+        if( !\is_null( $statusType ) ) {
+
+            $this->statusType = $statusType;
+        }
+
+        return $statusType;
+    }
+
+} ;
